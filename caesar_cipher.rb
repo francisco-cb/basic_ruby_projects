c8ffc72a1ba642c3fceade528edb891f35615631
Francisco Cubillos B <francisco.cubillos@ug.uchile.cl>
2022-03-13 20:06:10 -0300

    Change logic to be able to cycle back to start of loop
    In case of going over, makes it so that it cycles
    back to the start of the array and continues from there.
    
    With a key of 5 and the letter W for example, you would
    get the letter B, by going W -> X -> Y -> Z -> A -> B.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,10 +2,15 @@ def cipher(words='', key=0)
   char_list = words.split('')
   
   char_list.each_with_index do |char, indx|
-    # A-Z/a-z
-    if (char.ord <= 90 && 65 <= char.ord) || (char.ord <= 122 && 97 <= char.ord)
-      char_list[indx] = (char.ord + key).chr
+    if (char.ord <= 90 && 65 <= char.ord)
+      start_point = 65
+    elsif (char.ord <= 122 && 97 <= char.ord)
+      start_point = 97
+    else
+      next
     end
+
+    char_list[indx] = ((char.ord - start_point + key)%26 + start_point).chr
   end
 
   char_list.join('')
